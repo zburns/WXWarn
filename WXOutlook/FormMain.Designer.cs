571040d4e1d21b10e0fb51f3bc09ad6b778568da
Zachary Burns <developer@zackburns.com>
2016-10-11 19:01:09 -0400

    Add files via upload

---
+++ WXOutlook/FormMain.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WXOutlook
+{
+    partial class FormMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.labelCurrentTime = new System.Windows.Forms.Label();
+            this.timerClock = new System.Windows.Forms.Timer(this.components);
+            this.textBoxHazzardReport = new System.Windows.Forms.TextBox();
+            this.labelMorning = new System.Windows.Forms.Label();
+            this.labelAfternoon = new System.Windows.Forms.Label();
+            this.labelEvening = new System.Windows.Forms.Label();
+            this.checkBoxSendAlertToEmail = new System.Windows.Forms.CheckBox();
+            this.checkBoxPlayAlertIfHazzard = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // labelCurrentTime
+            // 
+            this.labelCurrentTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelCurrentTime.Location = new System.Drawing.Point(12, 9);
+            this.labelCurrentTime.Name = "labelCurrentTime";
+            this.labelCurrentTime.Size = new System.Drawing.Size(506, 23);
+            this.labelCurrentTime.TabIndex = 0;
+            this.labelCurrentTime.Text = "Current Time";
+            this.labelCurrentTime.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // timerClock
+            // 
+            this.timerClock.Tick += new System.EventHandler(this.timerClock_Tick);
+            // 
+            // textBoxHazzardReport
+            // 
+            this.textBoxHazzardReport.Location = new System.Drawing.Point(16, 76);
+            this.textBoxHazzardReport.Multiline = true;
+            this.textBoxHazzardReport.Name = "textBoxHazzardReport";
+            this.textBoxHazzardReport.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxHazzardReport.Size = new System.Drawing.Size(502, 228);
+            this.textBoxHazzardReport.TabIndex = 1;
+            // 
+            // labelMorning
+            // 
+            this.labelMorning.BackColor = System.Drawing.Color.Yellow;
+            this.labelMorning.Location = new System.Drawing.Point(13, 42);
+            this.labelMorning.Name = "labelMorning";
+            this.labelMorning.Size = new System.Drawing.Size(142, 23);
+            this.labelMorning.TabIndex = 2;
+            this.labelMorning.Text = "Morning (00:00 - 12:00)";
+            this.labelMorning.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelAfternoon
+            // 
+            this.labelAfternoon.BackColor = System.Drawing.Color.Yellow;
+            this.labelAfternoon.Location = new System.Drawing.Point(194, 41);
+            this.labelAfternoon.Name = "labelAfternoon";
+            this.labelAfternoon.Size = new System.Drawing.Size(142, 23);
+            this.labelAfternoon.TabIndex = 3;
+            this.labelAfternoon.Text = "Afternoon (13:00 - 17:00)";
+            this.labelAfternoon.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // labelEvening
+            // 
+            this.labelEvening.BackColor = System.Drawing.Color.Yellow;
+            this.labelEvening.Location = new System.Drawing.Point(376, 41);
+            this.labelEvening.Name = "labelEvening";
+            this.labelEvening.Size = new System.Drawing.Size(142, 23);
+            this.labelEvening.TabIndex = 4;
+            this.labelEvening.Text = "Evening (18:00 - 23:00)";
+            this.labelEvening.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // checkBoxSendAlertToEmail
+            // 
+            this.checkBoxSendAlertToEmail.AutoSize = true;
+            this.checkBoxSendAlertToEmail.Location = new System.Drawing.Point(16, 310);
+            this.checkBoxSendAlertToEmail.Name = "checkBoxSendAlertToEmail";
+            this.checkBoxSendAlertToEmail.Size = new System.Drawing.Size(115, 17);
+            this.checkBoxSendAlertToEmail.TabIndex = 5;
+            this.checkBoxSendAlertToEmail.Text = "Send Alert to Email";
+            this.checkBoxSendAlertToEmail.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxPlayAlertIfHazzard
+            // 
+            this.checkBoxPlayAlertIfHazzard.AutoSize = true;
+            this.checkBoxPlayAlertIfHazzard.Location = new System.Drawing.Point(187, 310);
+            this.checkBoxPlayAlertIfHazzard.Name = "checkBoxPlayAlertIfHazzard";
+            this.checkBoxPlayAlertIfHazzard.Size = new System.Drawing.Size(115, 17);
+            this.checkBoxPlayAlertIfHazzard.TabIndex = 6;
+            this.checkBoxPlayAlertIfHazzard.Text = "Play Alert if Hazard";
+            this.checkBoxPlayAlertIfHazzard.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(346, 310);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(172, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Play Alert if Skywarn Activation";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // FormMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(530, 333);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBoxPlayAlertIfHazzard);
+            this.Controls.Add(this.checkBoxSendAlertToEmail);
+            this.Controls.Add(this.labelEvening);
+            this.Controls.Add(this.labelAfternoon);
+            this.Controls.Add(this.labelMorning);
+            this.Controls.Add(this.textBoxHazzardReport);
+            this.Controls.Add(this.labelCurrentTime);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Hazardous Weather Outlook";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelCurrentTime;
+        private System.Windows.Forms.Timer timerClock;
+        private System.Windows.Forms.TextBox textBoxHazzardReport;
+        private System.Windows.Forms.Label labelMorning;
+        private System.Windows.Forms.Label labelAfternoon;
+        private System.Windows.Forms.Label labelEvening;
+        private System.Windows.Forms.CheckBox checkBoxSendAlertToEmail;
+        private System.Windows.Forms.CheckBox checkBoxPlayAlertIfHazzard;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ WXOutlook/FormMain.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WXOutlook
+{
+    public partial class FormMain : Form
+    {
+        private string HazardSite = "CLE";
+        private string StatementStart = "000";
+        private string StatementEnd = "$$";
+
+        private bool MorningChecked = false;
+        private bool AfternoonChecked = false;
+        private bool EveningChecked = false;
+
+        private string HazardousOutlook = null;
+
+        public FormMain()
+        {
+            InitializeComponent();
+
+            timerClock.Enabled = true;
+        }
+
+        private void timerClock_Tick(object sender, EventArgs e)
+        {
+            timerClock.Enabled = false;
+            labelCurrentTime.Text = System.DateTime.Now.ToString("HH:mm:ss");
+
+            if (!MorningChecked && System.DateTime.Now.Hour >= 0 && System.DateTime.Now.Hour <= 12)
+            {
+                HazardousOutlook = GetHazardousWeatherOutlook();
+                if (HazardousOutlook != null)
+                {
+                    if (HazardousOutlook.Length > 0)
+                    {
+                        MorningChecked = true;
+                        labelMorning.BackColor = Color.Green;
+                        textBoxHazzardReport.Text = HazardousOutlook;
+                        if (checkBoxSendAlertToEmail.Checked)
+                        {
+                            SendEmail(textBoxHazzardReport.Text);
+                        }
+                    }
+                    else
+                    {
+                        labelMorning.BackColor = Color.Red;
+                        textBoxHazzardReport.Text = "NWS - " + HazardSite + " Returned Invalid Outlook";
+                    }
+                }
+                else
+                {
+                    labelMorning.BackColor = Color.Red;
+                    textBoxHazzardReport.Text = "Error Retrieving Outlook from " + HazardSite;
+                }
+            }
+            else if (!AfternoonChecked && System.DateTime.Now.Hour >= 13 && System.DateTime.Now.Hour <= 17)
+            {
+                HazardousOutlook = GetHazardousWeatherOutlook();
+                if (HazardousOutlook != null)
+                {
+                    if (HazardousOutlook.Length > 0)
+                    {
+                        AfternoonChecked = true;
+                        labelAfternoon.BackColor = Color.Green;
+                        textBoxHazzardReport.Text = HazardousOutlook;
+                        if (checkBoxSendAlertToEmail.Checked)
+                        {
+                            SendEmail(textBoxHazzardReport.Text);
+                        }
+                    }
+                    else
+                    {
+                        labelAfternoon.BackColor = Color.Red;
+                        textBoxHazzardReport.Text = "NWS - " + HazardSite + " Returned Invalid Outlook";
+                    }
+                }
+                else
+                {
+                    labelAfternoon.BackColor = Color.Red;
+                    textBoxHazzardReport.Text = "Error Retrieving Outlook from " + HazardSite;
+                }
+            }
+            else if (!EveningChecked && System.DateTime.Now.Hour >= 18 && System.DateTime.Now.Hour <= 23)
+            {
+                HazardousOutlook = GetHazardousWeatherOutlook();
+                if (HazardousOutlook != null)
+                {
+                    if (HazardousOutlook.Length > 0)
+                    {
+                        EveningChecked = true;
+                        labelEvening.BackColor = Color.Green;
+                        textBoxHazzardReport.Text = HazardousOutlook;
+                        if (checkBoxSendAlertToEmail.Checked)
+                        {
+                            SendEmail(textBoxHazzardReport.Text);
+                        }
+                    }
+                    else
+                    {
+                        labelEvening.BackColor = Color.Red;
+                        textBoxHazzardReport.Text = "NWS - " + HazardSite + " Returned Invalid Outlook";
+                    }
+                }
+                else
+                {
+                    labelEvening.BackColor = Color.Red;
+                    textBoxHazzardReport.Text = "Error Retrieving Outlook from " + HazardSite;
+                }
+            }
+            else
+            {
+
+            }
+            timerClock.Enabled = true;
+        }
+
+        private string GetHazardousWeatherOutlook()
+        {
+            string Statement = null;
+            try
+            {
+                System.Net.WebRequest request = System.Net.WebRequest.Create("http://www.crh.noaa.gov/product.php?site=" + new System.Configuration.AppSettingsReader().GetValue("SiteCode", System.Type.GetType("System.String")) + "&issuedby=" + new System.Configuration.AppSettingsReader().GetValue("IssuedByCode", System.Type.GetType("System.String")) + "&product=" + new System.Configuration.AppSettingsReader().GetValue("ProductCode",System.Type.GetType("System.String")) + "&format=txt&version=1&glossary=0");
+                System.Net.WebResponse response = request.GetResponse();
+                System.IO.Stream responsestream = response.GetResponseStream();
+                System.IO.StreamReader responsereader = new System.IO.StreamReader(responsestream);
+                
+                Statement = responsereader.ReadToEnd();
+                
+                responsereader.Close();
+                responsereader.Dispose();
+                responsereader = null;
+                response.Close();
+                response = null;
+                request = null;
+
+                Statement = ParseStatement(Statement).Replace("\n","\r\n");  //fix unix to windows
+            }
+            catch
+            {
+                Statement = "Invalid Configuration Options Specified/Unable to Retrieve NWS Data";
+            }
+            return Statement;
+        }
+
+        private string ParseStatement(string Statement)
+        {
+            try
+            {
+                return Statement.Substring(Statement.IndexOf(StatementStart), Statement.IndexOf(StatementEnd) - Statement.IndexOf(StatementStart));
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        private void SendEmail(string AlertText)
+        {
+            try
+            {
+                System.Net.Mail.SmtpClient client = new System.Net.Mail.SmtpClient(new System.Configuration.AppSettingsReader().GetValue("SMTPServer", System.Type.GetType("System.String")).ToString(), Convert.ToInt32(new System.Configuration.AppSettingsReader().GetValue("SMTPPort", System.Type.GetType("System.String"))));
+                client.Credentials = new System.Net.NetworkCredential(new System.Configuration.AppSettingsReader().GetValue("SMTPUserName", System.Type.GetType("System.String")).ToString(), new System.Configuration.AppSettingsReader().GetValue("SMTPPassword", System.Type.GetType("System.String")).ToString());
+                client.DeliveryMethod = System.Net.Mail.SmtpDeliveryMethod.Network;
+                if (new System.Configuration.AppSettingsReader().GetValue("UseAuthentication", System.Type.GetType("System.String")).ToString().ToUpper() == "YES" || new System.Configuration.AppSettingsReader().GetValue("UseAuthentication", System.Type.GetType("System.String")).ToString().ToUpper() == "TRUE" || new System.Configuration.AppSettingsReader().GetValue("UseAuthentication", System.Type.GetType("System.String")).ToString().ToUpper() == "1")
+                    client.EnableSsl = true;
+                else
+                    client.EnableSsl = false;
+                client.SendAsync(new System.Net.Mail.MailMessage(new System.Configuration.AppSettingsReader().GetValue("SMTPUserName", System.Type.GetType("System.String")).ToString(), new System.Configuration.AppSettingsReader().GetValue("EmailAddress", System.Type.GetType("System.String")).ToString(), "NWS Hazard Statement", AlertText), null);
+            }
+            catch
+            {
+            }
+        }
+    }
+}
